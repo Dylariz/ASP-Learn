ccd341cbab36c8f28a9c7fc6c599fb143d4dbb51
Dylariz <zarec-k@mail.ru>
2022-11-26 15:51:23 +0300

    Migrate PizzaStore to SQLite

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using PizzaStore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -14,6 +16,7 @@ builder.Services.AddSwaggerGen(c =>
         Version = "v1"
     });
 });
+
 var app = builder.Build();
 
 app.UseSwagger();
